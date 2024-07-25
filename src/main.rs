6d929010f87c8a5982feb23d8b4b0abc794cbe4f
Santiago Pereira <97372983+SRV-JSPA@users.noreply.github.com>
2024-07-24 21:43:37 -0600

    Dibujo de poligono 2

--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,7 @@ fn main() {
     framebuffer.clear();
 
     dibujar_polygon1(&mut framebuffer);
+    dibujar_polygon2(&mut framebuffer);
 
     let _ = framebuffer.render_buffer("out.bmp");
 }
@@ -56,6 +57,24 @@ fn dibujar_polygon1(framebuffer: &mut Framebuffer) {
     framebuffer.polygon(&poly1); 
 
     
+}
+
+fn dibujar_polygon2(framebuffer: &mut Framebuffer) {
+    framebuffer.set_current_color(0xFF0000); 
+    let poligono2 = vec![
+        Vec3::new(321.0, 335.0, 0.0),
+        Vec3::new(288.0, 286.0, 0.0), 
+        Vec3::new(339.0, 251.0, 0.0),
+        Vec3::new(374.0, 302.0, 0.0)
+    ];
+    framebuffer.fill_polygon(&poligono2);
+
+    framebuffer.set_current_color(0xFFFFFF); 
+    let poly2 = vec![
+        (321, 335), (288, 286), (339, 251), (374, 302)
+    ];
+
+    framebuffer.polygon(&poly2);   
 }
 
 
