f9437620b4c2e80e26c475aac9155f1f27d9659e
Santiago Pereira <97372983+SRV-JSPA@users.noreply.github.com>
2024-07-24 21:50:27 -0600

    Dibujo poligono 4 y 5

--- src/main.rs
+++ src/main.rs
@@ -21,6 +21,8 @@ fn main() {
     dibujar_polygon1(&mut framebuffer);
     dibujar_polygon2(&mut framebuffer);
     dibujar_polygon3(&mut framebuffer);
+    dibujar_polygon4(&mut framebuffer);
+    dibujar_polygon5(&mut framebuffer);
 
     let _ = framebuffer.render_buffer("out.bmp");
 }
@@ -93,6 +95,60 @@ fn dibujar_polygon3(framebuffer: &mut Framebuffer) {
     ];
 
     framebuffer.polygon(&poly3); 
+
+    
+}
+
+fn dibujar_polygon4(framebuffer: &mut Framebuffer) {
+    framebuffer.set_current_color(0x00FF00); 
+    let poligono4 = vec![
+        Vec3::new(413.0, 177.0, 0.0),
+        Vec3::new(448.0, 159.0, 0.0), 
+        Vec3::new(502.0, 88.0, 0.0),
+        Vec3::new(553.0, 53.0, 0.0),
+        Vec3::new(535.0, 36.0, 0.0),
+        Vec3::new(676.0, 37.0, 0.0),
+        Vec3::new(660.0, 52.0, 0.0),
+        Vec3::new(750.0, 145.0, 0.0),
+        Vec3::new(761.0, 179.0, 0.0),
+        Vec3::new(672.0, 192.0, 0.0),
+        Vec3::new(659.0, 214.0, 0.0),
+        Vec3::new(615.0, 214.0, 0.0),
+        Vec3::new(632.0, 230.0, 0.0),
+        Vec3::new(580.0, 230.0, 0.0),
+        Vec3::new(597.0, 215.0, 0.0),
+        Vec3::new(552.0, 214.0, 0.0),
+        Vec3::new(517.0, 144.0, 0.0),
+        Vec3::new(466.0, 180.0, 0.0)
+    ];
+    framebuffer.fill_polygon(&poligono4);
+
+    framebuffer.set_current_color(0xFFFFFF); 
+    let poly4 = vec![
+        (413, 177), (448, 159), (502, 88), (553, 53), (535, 36), (676, 37), (660, 52),
+        (750, 145), (761, 179), (672, 192), (659, 214), (615, 214), (632, 230), (580, 230),
+        (597, 215), (552, 214), (517, 144), (466, 180)
+    ];
+
+    framebuffer.polygon(&poly4); 
+}
+
+fn dibujar_polygon5(framebuffer: &mut Framebuffer) {
+    framebuffer.set_current_color(0x000000); 
+    let poligono5 = vec![
+        Vec3::new(682.0, 175.0, 0.0),
+        Vec3::new(708.0, 120.0, 0.0), 
+        Vec3::new(735.0, 148.0, 0.0),
+        Vec3::new(739.0, 170.0, 0.0)
+    ];
+    framebuffer.fill_polygon(&poligono5);
+
+    framebuffer.set_current_color(0xFFFFFF); 
+    let poly5 = vec![
+        (682, 175), (708, 120), (735, 148), (739, 170)
+    ];
+
+    framebuffer.polygon(&poly5); 
 
     
 }
